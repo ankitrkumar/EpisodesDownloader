77a6b7ab2d7b68a613933908b9c663b62fb25113
ankitrkumar <ankitkumar1618@gmail.com>
2016-03-01 18:29:42 -0500

    Beta complete.

--- src/com/kumarankit/episodedownloader/Main.java
+++ src/com/kumarankit/episodedownloader/Main.java
@@ -15,40 +15,39 @@ public class Main {
     public static void main(String[] args) {
         //Sample for study : http://thewatchseries.to/episode/modern_family_s7_e14.html
 
-        String episodeURL ="";
+        String requestURL= "";
         if(args[1].equals("-l"))                     //ED "Modern Family" -l  #download the latest episode of this series
         {
-            episodeURL = constructLatestEpURL(args);
+            requestURL = constructLatestEpURL(args);
         }
         else if(args.length == 2 && !args[1].equals("-l"))                                         ////Input given as ED "Modern Family" s1-e14
         {
-            episodeURL = constructEpURL(args);
+            requestURL= Constants.WEBPAGE + "/" + Constants.EPISODE + "/" + constructEpURL(args)+ Constants.HTML.toLowerCase();
         }
         else{
             System.out.println("Input command arguments incorrect" +
                     "\n\n Use following format to download an episode when you know the exact episode::\n" +
-                    "EpisodesDownloader \"Modern Family\" s3-e12\n\n" +
+                    "EpisodesDownloader \"Modern Family\" s3e12\n\n" +
                     "To get the latest episode, use the following format::\n" +
                     "EpisodesDownloader \"Modern Family\" -l\n");
             return;
         }
 
         //create the url
-        String requestURL= Constants.WEBPAGE + "/" + Constants.EPISODE + "/" + episodeURL + Constants.HTML.toLowerCase();
-        System.out.println(requestURL);
+//        System.out.println(requestURL);
 
         Elements elem = makeRequest(requestURL);
 
         String encodedLink = getEncodedLink(elem);
-        System.out.println(encodedLink);
+//        System.out.println(encodedLink);
         String downloadLink = decodeBase64(encodedLink);
-        System.out.println(downloadLink);
+//        System.out.println(downloadLink);
 
-//        try {
-//            DownloadVideo(downloadLink);
-//        } catch (IOException e) {
-//            e.printStackTrace();
-//        }
+        try {
+            DownloadVideo(downloadLink);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
     private static String constructEpURL(String[] args) {
@@ -61,9 +60,18 @@ public class Main {
     }
 
     private static String constructLatestEpURL(String[] args) {
-        String retrievedURL = "";
-        
-        return parseSpaces(args[0]) + "_" + retrievedURL;
+        String pageURL = Constants.WEBPAGE + "/" + Constants.SERIES + "/" + parseSpaces(args[0]);
+        return getLatestEpisodeLink(makeRequest(pageURL));
+    }
+
+    private static String getLatestEpisodeLink(Elements elements) {
+        for (Element e : elements) {
+            if(e.text().contains("links)") && e.attr("abs:href").contains(Constants.WEBPAGE + "/" + Constants.EPISODE))
+            {
+                return e.attr("abs:href");
+            }
+        }
+        return "";
     }
 
     private static String decodeBase64(String encodedString)
@@ -96,10 +104,6 @@ public class Main {
             }
         }
         return null;
-    }
-
-    private static void print(String msg, Object... args) {
-        System.out.println(String.format(msg, args));
     }
 
     private static void DownloadVideo(String url) throws IOException {
