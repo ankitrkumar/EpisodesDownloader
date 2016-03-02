9e76e6214a4470ec7551b4e223971db389b60c81
ankitrkumar <ankitkumar1618@gmail.com>
2016-03-01 21:34:26 -0500

    Polished and tested

--- src/com/kumarankit/episodedownloader/Main.java
+++ src/com/kumarankit/episodedownloader/Main.java
@@ -11,43 +11,50 @@ import java.io.InputStreamReader;
 import java.util.Base64;
 
 public class Main {
+    static boolean download_success = false;
+    static int failed_count = 0;
 
     public static void main(String[] args) {
         //Sample for study : http://thewatchseries.to/episode/modern_family_s7_e14.html
 
-        String requestURL= "";
-        if(args[1].equals("-l"))                     //ED "Modern Family" -l  #download the latest episode of this series
-        {
-            requestURL = constructLatestEpURL(args);
-        }
-        else if(args.length == 2 && !args[1].equals("-l"))                                         ////Input given as ED "Modern Family" s1-e14
-        {
-            requestURL= Constants.WEBPAGE + "/" + Constants.EPISODE + "/" + constructEpURL(args)+ Constants.HTML.toLowerCase();
-        }
-        else{
-            System.out.println("Input command arguments incorrect" +
-                    "\n\n Use following format to download an episode when you know the exact episode::\n" +
-                    "EpisodesDownloader \"Modern Family\" s3e12\n\n" +
-                    "To get the latest episode, use the following format::\n" +
-                    "EpisodesDownloader \"Modern Family\" -l\n");
-            return;
-        }
+        try{
+            while(!download_success) {
+                String requestURL = "";
+                if (args[1].equals("-l"))                     //ED "Modern Family" -l  #download the latest episode of this series
+                {
+                    requestURL = constructLatestEpURL(args);
+                } else if (args.length == 2 && !args[1].equals("-l"))                                         ////Input given as ED "Modern Family" s1-e14
+                {
+                    requestURL = Constants.WEBPAGE + "/" + Constants.EPISODE + "/" + constructEpURL(args) + Constants.HTML.toLowerCase();
+                } else {
+                    System.out.println("Input command arguments incorrect" +
+                            "\n\n Use following format to download an episode when you know the exact episode::\n" +
+                            "EpisodesDownloader \"Modern Family\" s3e12\n\n" +
+                            "To get the latest episode, use the following format::\n" +
+                            "EpisodesDownloader \"Modern Family\" -l\n");
+                    return;
+                }
 
-        //create the url
-//        System.out.println(requestURL);
+                //create the url
+//                System.out.println(requestURL);
 
-        Elements elem = makeRequest(requestURL);
+                Elements elem = makeRequest(requestURL);
 
-        String encodedLink = getEncodedLink(elem);
-//        System.out.println(encodedLink);
-        String downloadLink = decodeBase64(encodedLink);
-//        System.out.println(downloadLink);
+                String encodedLink = getEncodedLink(elem);
+//                System.out.println(encodedLink);
+                String downloadLink = decodeBase64(encodedLink);
+//                System.out.println(downloadLink);
 
-        try {
-            DownloadVideo(downloadLink);
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+                try {
+                    DownloadVideo(downloadLink);
+                } catch (IOException e) {
+                    System.out.println("Download attempt failed from link " + failed_count + ". Retrying...");
+                }
+            }
+    }catch(Exception e)
+    {
+        System.out.println("\nError in fetching and downloading file. Verify if the episode exists.");
+    }
     }
 
     private static String constructEpURL(String[] args) {
@@ -90,17 +97,24 @@ public class Main {
         try {
             doc = Jsoup.connect(constructedURL).get();
         } catch (IOException e) {
-            e.printStackTrace();
+            System.out.println("The requested show isn't available.");
         }
-        return doc.select("a[href]");
+        return doc != null ? doc.select("a[href]") : null;
     }
 
     private static String getEncodedLink(Elements e)
     {
+        int skip_count = failed_count;
         for (Element link : e) {
             if(link.attr("abs:href").contains("cale.html?r"))
             {
-                return link.attr("href").replace("?","").substring(12);
+                if(skip_count == 0) {
+                    return link.attr("href").replace("?", "").substring(12);
+                }
+                else {
+                    skip_count--;
+                    continue;
+                }
             }
         }
         return null;
@@ -114,12 +128,22 @@ public class Main {
         Process p = builder.start();
         BufferedReader r = new BufferedReader(new InputStreamReader(p.getInputStream()));
         String line;
+        System.out.println("\n\n");
         while (true) {
             line = r.readLine();
             if (line == null) {
                 break;
             }
-            System.out.println(line);
+            if (!line.contains("ERROR"))
+                System.out.print(line + "\r");
+            if(line.contains("ERROR"))
+            {
+                download_success = false;
+                failed_count++;
+            }
+            else if (line.contains("100%")){
+                download_success = true;
+            }
         }
     }
 }
